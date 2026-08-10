2d8b8676eef53ce6302e9678c82ce72585f716a5
Ahmed A. <115487372+AhmAbd@users.noreply.github.com>
2026-08-10 10:29:12 +0300

    Question 4 Solution
    since both getUser and getOrders functions are async, we need to use an async function and await while trying to call the functions.
    
    used try, catch to catch any errors while trying to call the functions and fetch user and order data
    
    if there are no orders, make the list an empty array, so that we can run the sum loop in the next lines.
    
    then return the username, ordersCount, totalSpent

--- Question4.js
+++ Question4.js
@@ -21,6 +21,35 @@ async function getOrders(userId){
 
 }
 
+async function getUserSummary() {
+    try {
+        const user = await getUser();
+    } catch (error) {
+        console.log("حصل خطأ في الحصول على بيانات المستخدم", error)
+        return;
+    }
+
+    try {
+        let ordersList = await getOrders(user.id)
+    } catch (error) {
+        console.log("حصل خطأ في الحصول على طلبات المستخدم", error)
+        return;
+    }
+    
+    if (!ordersList) {
+        ordersList = [];
+    }
+    let totalSpent = 0;
+    for(const order of ordersList) {
+        totalSpent += order.amount;
+    }
+    return {
+        user: user.name,
+        ordersCount: ordersList.length,
+        totalSpent
+    }
+}
+
 /*
 اكتب دالة getUserSummary()
 
